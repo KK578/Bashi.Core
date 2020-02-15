2c3232a2e6dc58f07352cad7614565a4da022c5d
Kevin Kwan <KevinKwan95@googlemail.com>
2020-02-15 22:58:22 +0000

    BASHI-9: Migrate Bashi.Core.Tests | Move to multi-repo format

---
+++ test/Bashi.Core.Tests/TinyTypes/TinyStringTests.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Globalization;
+using Bashi.Core.TinyTypes;
+using Bashi.Tests.Framework.Data;
+using NUnit.Framework;
+
+namespace Bashi.Core.Tests.TinyTypes
+{
+    [TestFixture]
+    public class TinyStringTests
+    {
+        [Test]
+        public void CompareTo_GivenNull_ThenShouldThrowException()
+        {
+            var tt = new MyTinyString(TestData.WellKnownString);
+            Assert.That(() => tt.CompareTo((object?)null), Throws.ArgumentNullException);
+            Assert.That(() => tt.CompareTo((MyTinyString?)null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Equals_GivenStringsWithDifferentCases_WhenStringComparerIgnoresCase_ThenShouldBeTrue()
+        {
+            var tt = new MyTinyString(TestData.WellKnownString);
+            var upperTt = new MyTinyString(TestData.WellKnownString.ToUpper(CultureInfo.InvariantCulture));
+            Assert.That(tt.Equals(upperTt), Is.True);
+        }
+
+        [Test]
+        public void GetHashCode_GivenStringsWithDifferentCases_WhenStringComparerIgnoresCase_ThenShouldBeTrue()
+        {
+            var tt = new MyTinyString(TestData.WellKnownString);
+            var upperTt = new MyTinyString(TestData.WellKnownString.ToUpper(CultureInfo.InvariantCulture));
+            Assert.That(tt.GetHashCode(), Is.EqualTo(upperTt.GetHashCode()));
+        }
+
+        [Test]
+        public void CompareTo_GivenStringWithDifferentCases_WhenStringComparerIgnoresCase_ThenShouldBeZero()
+        {
+            var tt = new MyTinyString(TestData.WellKnownString);
+            var upperTt = new MyTinyString(TestData.WellKnownString.ToUpper(CultureInfo.InvariantCulture));
+            Assert.That(tt.CompareTo(upperTt), Is.Zero);
+        }
+
+        [Test]
+        public void Equals_GivenStringsWithDifferentCases_WhenStringComparerIsCaseSensitive_ThenShouldBeFalse()
+        {
+            var tt = new MySensitiveTinyString(TestData.WellKnownString);
+            var upperTt = new MySensitiveTinyString(TestData.WellKnownString.ToUpper(CultureInfo.InvariantCulture));
+            Assert.That(tt.Equals(upperTt), Is.False);
+        }
+
+        [Test]
+        public void GetHashCode_GivenStringsWithDifferentCases_WhenStringComparerIsCaseSensitive_ThenShouldBeFalse()
+        {
+            var tt = new MySensitiveTinyString(TestData.WellKnownString);
+            var upperTt = new MySensitiveTinyString(TestData.WellKnownString.ToUpper(CultureInfo.InvariantCulture));
+            Assert.That(tt.GetHashCode(), Is.Not.EqualTo(upperTt.GetHashCode()));
+        }
+
+        [Test]
+        public void CompareTo_GivenStringWithDifferentCases_WhenStringComparerIsCaseSensitive_ThenShouldBeZero()
+        {
+            var tt = new MySensitiveTinyString(TestData.WellKnownString);
+            var upperTt = new MySensitiveTinyString(TestData.WellKnownString.ToUpper(CultureInfo.InvariantCulture));
+            Assert.That(tt.CompareTo(upperTt), Is.Not.Zero);
+        }
+
+        private class MyTinyString : TinyString
+        {
+            public MyTinyString(string value)
+                : base(value)
+            {
+            }
+        }
+
+        private class MySensitiveTinyString : TinyString
+        {
+            public MySensitiveTinyString(string value)
+                : base(value, StringComparer.Ordinal)
+            {
+            }
+        }
+    }
+}
