706f214ed2495d5a7d992ed237d0a94500e4ce79
KK578 <KevinKwan95@googlemail.com>
2020-04-11 14:26:57 +0100

    BASHI-35: Add GetDescription and strongly-typed GetValues | Add Basic Enum Utilities

---
+++ src/Bashi.Core/Enums/EnumUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bashi.Core.Enums
+{
+    /// <summary>
+    /// Provides various static utility methods to be used on <see cref="System.Enum"/> values.
+    /// </summary>
+    public static class EnumUtil
+    {
+        /// <summary>
+        /// Retrieves a strongly-typed read-only list of the values of the constants in the specified enumeration <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Enum type to get the values of.</typeparam>
+        /// <returns>All values in the specified enum.</returns>
+        public static IReadOnlyList<T> GetValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>().ToList().AsReadOnly();
+        }
+    }
+}
---
+++ test/Bashi.Core.Tests/Enums/EnumUtilTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Bashi.Core.Enums;
+using NUnit.Framework;
+
+namespace Bashi.Core.Tests.Enums
+{
+    [TestFixture]
+    public class EnumUtilTests
+    {
+        private enum TestColour
+        {
+            Red,
+            Green,
+            Blue,
+        }
+
+        [Test]
+        public void GetValues_ShouldReturnAllEnumMembers()
+        {
+            var expected = new List<TestColour> { TestColour.Red, TestColour.Green, TestColour.Blue };
+            Assert.That(EnumUtil.GetValues<TestColour>(), Is.EqualTo(expected).AsCollection);
+        }
+    }
+}
