ca324288bfd6523e80f677f4769083e00914b1f2
KK578 <KevinKwan95@googlemail.com>
2020-12-05 21:38:56 +0000

    BASHI-92: Add NullToEmpty | Add Enumerable Extensions

---
+++ src/Bashi.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Bashi.Core.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Returns the <paramref name="source"/> enumerable unchanged or an empty enumerable if <paramref name="source"/> is null.
+        /// </summary>
+        /// <param name="source">The enumerable to be null-checked.</param>
+        /// <typeparam name="T">The type of the elements of <paramref name="source"/>.</typeparam>
+        /// <returns>The <paramref name="source"/> enumerable if present, else a new empty enumerable.</returns>
+        public static IEnumerable<T> NullToEmpty<T>(this IEnumerable<T>? source)
+        {
+            return source ?? Enumerable.Empty<T>();
+        }
+    }
+}
---
+++ test/Bashi.Core.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bashi.Core.Extensions;
+using NUnit.Framework;
+
+namespace Bashi.Core.Tests.Extensions
+{
+    [TestFixture]
+    public class EnumerableExtensionsTests
+    {
+        [Test]
+        public void NullToEmpty_GivenNull_ThenReturnsEmpty()
+        {
+            IEnumerable<string>? source = null;
+            Assert.That(source.NullToEmpty(), Is.Not.Null);
+        }
+
+        [Test]
+        public void NullToEmpty_GivenNonNull_ThenReturnsSameInstance()
+        {
+            IEnumerable<string> source = Enumerable.Empty<string>();
+            IEnumerable<int> source2 = Enumerable.Range(0, 5);
+            Assert.That(source.NullToEmpty(), Is.EqualTo(source));
+            Assert.That(source2.NullToEmpty(), Is.EqualTo(source2));
+        }
+    }
+}
