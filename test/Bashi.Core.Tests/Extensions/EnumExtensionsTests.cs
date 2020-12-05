3d97e89446c06fff50ad1c176ec15742bf13e42c
Kevin Kwan <KevinKwan95@googlemail.com>
2020-12-05 23:26:22 +0000

    BASHI-92: Add Enumerable Extensions. (#27)

---
+++ test/Bashi.Core.Tests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Bashi.Core.Extensions;
+using Bashi.Core.Tests.Utils;
+using NUnit.Framework;
+
+namespace Bashi.Core.Tests.Extensions
+{
+    [TestFixture]
+    public class EnumExtensionsTests
+    {
+        [Test]
+        public void GetDescription_GivenMemberDoesNotHaveDescriptionAttribute_ShouldReturnToString()
+        {
+            Assert.That(EnumUtilTests.TestColour.Yellow.GetDescription(), Is.EqualTo("Yellow"));
+        }
+
+        [Test]
+        public void GetDescription_GivenMemberDoesHaveDescriptionAttribute_ShouldReturnDescriptionValue()
+        {
+            Assert.That(EnumUtilTests.TestColour.Red.GetDescription(), Is.EqualTo("#FF0000"));
+            Assert.That(EnumUtilTests.TestColour.Green.GetDescription(), Is.EqualTo("#00FF00"));
+            Assert.That(EnumUtilTests.TestColour.Blue.GetDescription(), Is.EqualTo("#0000FF"));
+        }
+    }
+}
