760d6cb9f19bd26fec9de71272292c8542fa5aac
Kevin Kwan <KevinKwan95@googlemail.com>
2021-06-24 23:12:06 +0100

    BASHI-109: [Breaking] Assorted Utilities & Remove Implicit Type Cast of TinyTypes. (#45)

--- test/Bashi.Core.Tests/Extensions/EnumerableExtensionsTests.cs
+++ test/Bashi.Core.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -8,20 +8,67 @@ namespace Bashi.Core.Tests.Extensions
     [TestFixture]
     public class EnumerableExtensionsTests
     {
-        [Test]
-        public void NullToEmpty_GivenNull_ThenReturnsEmpty()
+        public class AsReadOnlyListTests
         {
-            IEnumerable<string>? source = null;
-            Assert.That(source.NullToEmpty(), Is.Not.Null);
+            [Test]
+            public void GivenEnumerable_ThenReturnsNewList()
+            {
+                var enumerable = Enumerable.Range(1, 10);
+                Assert.That(enumerable.AsReadOnlyList(), Is.Not.SameAs(enumerable));
+            }
+
+            [Test]
+            public void GivenList_ThenReturnsNewList()
+            {
+                var list = Enumerable.Range(1, 10).ToList();
+                Assert.That(list.AsReadOnlyList(), Is.Not.SameAs(list));
+            }
+
+            [Test]
+            public void GivenReadOnlyList_ThenReturnsExistingList()
+            {
+                var readonlyList = Enumerable.Range(1, 10).ToList().AsReadOnly();
+                Assert.That(readonlyList.AsReadOnlyList(), Is.SameAs(readonlyList));
+            }
         }
 
-        [Test]
-        public void NullToEmpty_GivenNonNull_ThenReturnsSameInstance()
+        public class NullToEmptyTests
         {
-            IEnumerable<string> source = Enumerable.Empty<string>();
-            IEnumerable<int> source2 = Enumerable.Range(0, 5);
-            Assert.That(source.NullToEmpty(), Is.EqualTo(source));
-            Assert.That(source2.NullToEmpty(), Is.EqualTo(source2));
+            [Test]
+            public void GivenNull_ThenReturnsEmpty()
+            {
+                IEnumerable<string>? source = null;
+                Assert.That(source.NullToEmpty(), Is.Not.Null);
+            }
+
+            [Test]
+            public void GivenNonNull_ThenReturnsSameInstance()
+            {
+                IEnumerable<string> source = Enumerable.Empty<string>();
+                IEnumerable<int> source2 = Enumerable.Range(0, 5);
+                Assert.That(source.NullToEmpty(), Is.SameAs(source));
+                Assert.That(source2.NullToEmpty(), Is.SameAs(source2));
+            }
+        }
+
+        public class FirstRandomTests
+        {
+            [Test]
+            public void GivenEmptyList_ThenReturnsDefault()
+            {
+                var empty = Enumerable.Empty<int>();
+                Assert.That(empty.FirstRandom(), Is.EqualTo(0));
+            }
+
+            [Test]
+            public void GivenListOf10Items_ThenReturnsAnItemFromCollectionEachTime()
+            {
+                var list = Enumerable.Range(1, 10).ToList();
+                for (var i = 0; i < 100; i++)
+                {
+                    Assert.That(list, Contains.Item(list.FirstRandom()));
+                }
+            }
         }
     }
 }
